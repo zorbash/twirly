804454aee896e0a2c9da4b97fd3fae3bb72d3670
Dimitris Zorbas <zorbash@skroutz.gr>
2014-12-07 22:55:59 +0200

    Add Twirly::Post

---
+++ lib/twirly/post.rb
@@ -0,0 +1,81 @@
+module Twirly
+  class Post < SimpleDelegator
+    include Twirly::Serializers::Post
+
+    define_method(:alias_attribute, &Trello::Card.method(:alias_attribute).to_proc)
+
+    alias :card :__getobj__
+
+    alias_attribute :title, :name
+    alias_attribute :updated_at, :last_activity_date
+    alias_attribute :created_at, :due
+    alias_attribute :published_at, :due
+    alias_attribute :card_id, :short_id
+
+    def category; card.list.name end
+
+    def tags
+      labels.map(&:name).reject { |label| label == 'published' }
+    end
+
+    def existing_front_matter
+      @existing_front_matter ||= card.desc.scan(/(---.*---)/mi).flatten.first
+    end
+
+    def processed_front_matter
+      @processed_front_matter =
+      begin
+        matter = YAML::load(existing_front_matter || '{}')
+        matter[:title] = title
+        matter[:slug] = slug
+        matter[:category] = category
+        matter[:date] = short_published_at
+        matter[:tags] = tags.join(', ')
+        matter[:authors] = authors.map(&:username).join(', ')
+        matter[:attachments] = attachments.map(&:url).join(', ') if card.attachments.any?
+        matter
+      end
+    end
+
+    def body
+      @body ||= Liquid::Template.parse(card.desc.sub(/---.*---/mi, '').strip).render
+    end
+
+    def short_published_at
+      published_at.strftime('%Y-%m-%d')
+    end
+
+    def published?
+      published_at && card.labels.any? { |label| label.name == 'published' }
+    end
+
+    def slug
+      "#{short_published_at}-#{title.parameterize}-#{card_id}"
+    end
+
+    def authors
+      members.map { |member| Twirly::User.new(member) }
+    end
+
+    def front_matter
+      "#{processed_front_matter.to_yaml}\n---"
+    end
+
+    def to_md
+      "#{front_matter}\n\n#{body}"
+    end
+
+    class << self
+      def published
+        Twirly.board.cards.map { |card|
+          post = Post.new(card)
+          if post.published? then post else nil end
+        }.compact.sort { |p1, p2| p2.published_at <=> p1.published_at }
+      end
+
+      def find(id)
+        Post.new(Twirly.board.cards.find { |card| card.short_id == id })
+      end
+    end
+  end
+end
---
+++ spec/post_spec.rb
@@ -0,0 +1,155 @@
+require 'spec_helper'
+
+describe Twirly::Post do
+  let(:trello_board) { create(:trello_board) }
+  let(:trello_card) { create(:trello_card) }
+  let(:front_matter_content) do
+    <<-YAML.strip_heredoc.strip
+    ---
+    important: true
+    tags: idm, punk, electro
+    ---
+    YAML
+  end
+
+  let(:markdown_content) do
+    <<-MDN.strip_heredoc.strip
+    # A post about cats
+    > Everybody loves cats
+    {% youtube 320 280 4-IpnaqBhT4 %}
+    MDN
+  end
+
+  let(:post) { Twirly::Post.new(trello_card) }
+  subject { post }
+
+  describe '#card' do
+    it 'returns a Trello::Card' do
+      expect(subject.card.class).to eq(Trello::Card)
+    end
+  end
+
+  describe '#title' do
+    it "is delegated to the card's name" do
+      expect(subject.title).to eq(subject.card.name)
+    end
+  end
+
+  describe '#updated_at' do
+    it "is delegated to the card's last_activity_date" do
+      expect(subject.updated_at).to eq(subject.card.last_activity_date)
+    end
+  end
+
+  describe '#created_at' do
+    it "is delegated to the card's due" do
+      expect(subject.created_at).to eq(subject.card.due)
+    end
+  end
+
+  describe '#published_at' do
+    it "is delegated to the card's due" do
+      expect(subject.published_at).to eq(subject.card.due)
+    end
+  end
+
+  describe '#card_id' do
+    it "is delegated to the card's id" do
+      expect(subject.card_id).to eq(subject.card.short_id)
+    end
+  end
+
+  describe '#short_published_at' do
+    subject { post.short_published_at }
+
+    it { expect(subject).to eq(trello_card.due.strftime('%Y-%m-%d')) }
+  end
+
+  describe '#slug' do
+    subject { post.slug }
+    let(:slug) {"#{post.short_published_at}-#{post.title.parameterize}-#{post.card_id}" }
+
+    it { expect(subject).to eq(slug) }
+  end
+
+  describe '#authors' do
+    before do
+      subject.card.stub(:members).and_return(create_list(:trello_member, 3))
+    end
+
+    it "returns the decorated as Twirly::User(s) card members" do
+      expect(subject.authors.map(&:username))
+        .to match_array(subject.card.members.map(&:username))
+    end
+  end
+
+  describe '#tags' do
+    pending
+  end
+
+  describe '#body' do
+    pending
+  end
+
+  describe '#published?' do
+    context 'card is published (has published_at and the "published" label' do
+      let(:subject) do
+        post = Twirly::Post.new(create(:trello_card, :published))
+        post.card.stub(:labels).and_return([double(name: 'published')])
+        post
+      end
+
+      it 'returns true' do
+        expect(subject).to be_published
+      end
+    end
+
+    context 'card is not published (has published_at and the "published" label' do
+      let(:subject) { Twirly::Post.new(create(:trello_card, :unpublished)) }
+
+      it 'returns false' do
+        expect(subject).to_not be_published
+      end
+    end
+  end
+
+  describe '.published' do
+    let(:unpublished_cards) do
+      create_list(:trello_card, 3, :unpublished).map do |card|
+        card.stub(:labels).and_return([])
+        card
+      end
+    end
+
+    let(:published_cards) do
+      create_list(:trello_card, 4, :published).map do |card|
+        card.stub(:labels).and_return([double(name: 'published')])
+        card
+      end
+    end
+
+    before do
+      Trello::Board.stub(:find).and_return(trello_board)
+      Twirly.board.stub(:cards).and_return(unpublished_cards + published_cards)
+    end
+
+    it 'returns only posts decorating published cards' do
+      expect(Twirly::Post.published.map(&:card_id)).
+        to match_array(published_cards.map(&:short_id))
+    end
+  end
+
+  describe '.find' do
+    let(:matching_card) { create(:trello_card, short_id: 42) }
+    let(:non_matching_card) { create(:trello_card, short_id: 404) }
+
+    before do
+      Trello::Board.stub(:find).and_return(trello_board)
+      Twirly.board.stub(:cards).and_return([matching_card, non_matching_card])
+    end
+
+    it 'returns a card from the registered board with a matching trello short_id' do
+      expect(Twirly::Post.find(42).card_id).to eq(42)
+    end
+  end
+end
