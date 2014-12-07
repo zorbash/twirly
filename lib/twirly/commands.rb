9d648c6997671a532102b9c04388d15840c30ca5
Dimitris Zorbas <zorbash@skroutz.gr>
2014-12-07 23:38:09 +0200

    Autoload Twirly::Commands::FetchUser

--- lib/twirly/commands.rb
+++ lib/twirly/commands.rb
@@ -2,4 +2,5 @@ module Twirly::Commands
   autoload :Base, 'twirly/commands/base'
   autoload :Pull, 'twirly/commands/pull'
   autoload :Pick, 'twirly/commands/pick'
+  autoload :FetchUser, 'twirly/commands/fetch_user'
 end
