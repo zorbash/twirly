9eb83a82a67f9db2d2c652dcc4a3573fa3bf3126
Dimitris Zorbas <zorbash@skroutz.gr>
2014-12-07 23:06:56 +0200

    Add Twirly::Compiler#pick

--- lib/twirly/compiler.rb
+++ lib/twirly/compiler.rb
@@ -8,5 +8,12 @@ module Twirly
         File.open(output_path, 'w') { |out| out << post.to_md }
       end
     end
+
+    def pick(post)
+      output_path = File.join Twirly.source_dir, "#{post.slug}.html.md.erb"
+      puts "Creating #{output_path}"
+
+      File.open(output_path, 'w') { |out| out << post.to_md }
+    end
   end
 end
